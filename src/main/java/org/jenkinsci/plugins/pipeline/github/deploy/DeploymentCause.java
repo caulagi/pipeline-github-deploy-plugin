3af601534c7538c72834ae8d05959d53a910c4fe
Pradip Caulagi <caulagi@gmail.com>
2018-08-02 22:21:19 +0200

    fix: incorrect descriptor implementation

--- src/main/java/org/jenkinsci/plugins/pipeline/github/deploy/DeploymentCause.java
+++ src/main/java/org/jenkinsci/plugins/pipeline/github/deploy/DeploymentCause.java
@@ -2,15 +2,9 @@ package org.jenkinsci.plugins.pipeline.github.deploy;
 
 import static java.lang.String.format;
 
-import org.jenkinsci.Symbol;
 import org.kohsuke.github.GHEventPayload;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import hudson.Extension;
 import hudson.model.Cause;
-import hudson.model.Item;
-import hudson.triggers.TriggerDescriptor;
 
 public class DeploymentCause extends Cause {
 
@@ -25,17 +19,4 @@ public class DeploymentCause extends Cause {
         return format("Started by GitHub deployment for repo:%s, branch:%s", deploymentEvent.getRepository().getName(),
                 deploymentEvent.getDeployment().getRef());
     }
-
-    @Symbol("githubDeployment")
-    @Extension
-    public static class DescriptorImpl extends TriggerDescriptor {
-
-        @Override
-        public boolean isApplicable(Item item) {
-            LOGGER.info("TRIGGER: {}", item);
-            return false;
-        }
-    }
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DeploymentEventSubscriber.class);
 }
